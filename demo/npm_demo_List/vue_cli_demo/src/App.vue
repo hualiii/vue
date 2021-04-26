199cadf2419c5c8d02fa7da305d580b94355fe0b
QHL <qhl@lbx.dev>
2021-04-26 09:41:02 +0800

    add todo list demo

---
+++ demo/npm_demo_List/vue_cli_demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="info">
+    <button @click="addInfo">添加</button>
+    <ul>
+      <TodoItem v-for="(item,index) in list" :item="item" :key="index"></TodoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      info: "",
+      list: []
+    }
+  },
+  methods: {
+    addInfo() {
+      this.list.push(this.info)
+      this.info = ''
+    }
+  },
+  components: {
+    // HelloWorld
+    TodoItem
+  }
+}
+</script>
+
+<style>
+#app {
+}
+</style>
---
+++ demo/npm_demo_List/vue_cli_demo/src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="item">{{ item }}</li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ['item']
+}
+</script>
+
+<style scoped>
+.item {
+  color: red;
+}
+</style>
