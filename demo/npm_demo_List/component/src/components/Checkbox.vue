471a064acf5d81129d72b3d9c78c8d444aa4e087
QHL <qhl@lbx.dev>
2021-04-27 10:48:46 +0800

    add checkbox text select component v-model demo

---
+++ demo/npm_demo_List/component/src/components/Checkbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>checkbox v-model demo</h2>
+    <input type="checkbox" v-bind:checked="checked"
+           v-on:change="$emit('change',$event.target.checked)"><span>{{ checked }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "checkbox",
+  model: {
+    prop: "checked",
+    event: "change"
+  },
+  props: {
+    checked: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo/npm_demo_List/component/src/components/InputText.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>input v-model demo</h2>
+    <input type="text" v-bind:value="value" v-on:input="$emit('input',$event.target.value)">
+    <div>{{ value }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputText",
+  model: {
+    prop: 'value',
+    event: 'input'
+  },
+  props: ['value']
+}
+</script>
+
+<style scoped>
+
+</style>
