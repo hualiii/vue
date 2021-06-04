d17b6a7c4530926acdb834d294a9dad48fe3c487
qhl <qhl@lbx.dev>
2021-06-04 15:04:32 +0800

    add user route

---
+++ src/layouts/UserLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>Ant-Design-Vue-Pro</div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserLayout",
+};
+</script>
+
+<style scoped></style>
