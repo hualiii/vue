d4d11a9c3c5a6297e66e220233e929bb5710f289
QHL <qhl@lbx.dev>
2021-09-17 18:02:45 +0800

    添加router笔记

--- vue_test/src/router/index.js
+++ vue_test/src/router/index.js
@@ -5,11 +5,20 @@ const router = new VueRouter({
   mode: 'history',
   routes: [
     {
+      name: 'home',
       path: '/home',
       component: Home,
+      meta: {
+        title: "主页"
+      },
       children: [
         {
+          name: 'news',
           path: "news",
+          meta: {
+            title: "新闻",
+            isLock: true,
+          },
           component: () => import('../views/News.vue'),
           children: [
             {
@@ -23,10 +32,16 @@ const router = new VueRouter({
           ]
         },
         {
+          name: 'messages',
           path: "messages",
+          meta: {
+            title: "消息",
+            isLock: true,
+          },
           component: () => import('../views/Messages.vue'),
           children: [
             {
+              name: 'detail',
               path: "detail",
               component: () => import('../views/Detail.vue'),
               // props的第三种种写法,props值为函数,函数返回值以props传给路由组件Detail,函数可以接收$route参数
@@ -43,8 +58,25 @@ const router = new VueRouter({
     },
     {
       path: '/about',
-      component: About
+      component: About,
+      meta: {
+        title: "关于"
+      }
     }
   ]
+})
+router.beforeEach((to, from, next) => {
+  let username = "admin"
+  if (to.meta.isLock) {
+    if (username === "admin1") {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+router.afterEach(route => {
+  console.log(route)
+  document.getElementsByTagName('title')[0].innerText = route.meta.title
 })
 export default router
