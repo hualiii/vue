c4977ca33e9640c6c0e68582ec85219ed72f4f31
QHL <qhl@lbx.dev>
2021-10-11 18:05:36 +0800

    dd

---
+++ vue3_test/2.vue3_hook函数的基本使用/components/Demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>当前求和为:{{ sum }}</h2>
+    <button @click="sum++">add</button>
+    <hr />
+    <h2>当前坐标点为:x:{{ point.x }},y:{{ point.y }}</h2>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import userPoint from "../hooks/usePoint";
+export default {
+  name: "Demo",
+  setup() {
+    let sum = ref(0);
+    let point = userPoint();
+    return {
+      sum,
+      point,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue3_test/src/components/Demo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>{{ person }}</p>
+    <h1>roRef</h1>
+    <h2>name:{{ name }}</h2>
+    <h2>age:{{ age }}</h2>
+    <h2>salary:{{ salary }} k</h2>
+    <button @click="salary++">salary++</button>
+    <button @click="age++">age++</button>
+    <button @click="name += '~'">name++</button>
+  </div>
+</template>
+
+<script>
+import { reactive, toRef } from "vue";
+export default {
+  name: "Demo",
+  setup() {
+    let person = reactive({
+      name: "张三",
+      age: 18,
+      job: {
+        type: "web前端工程师",
+        salary: 18,
+      },
+    });
+    return {
+      person,
+      name: toRef(person, "name"),
+      age: toRef(person, "age"),
+      salary: toRef(person.job, "salary"),
+    };
+  },
+};
+</script>
+
+<style>
+</style>
