cf67b471d7b62686076c134c33f3c120b05d88bf
QHL <qhl@lbx.dev>
2021-11-10 15:50:50 +0800

    des

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.header {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 33.5%;
+  color: #fff;
+  font-size: 0.28rem;
+  text-align: center;
+  background-image: url("../assets/images/bg1.jpg");
+  background-size: cover;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="home">
-    <h1>home</h1>
+    <Header />
+    <NcovInfo :infoData="ncovInfo" />
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import Header from "../components/Header";
+import NcovInfo from "../components/NcovInfo";
 export default {
   name: "Home",
+  data() {
+    return {
+      ncovInfo: {},
+    };
+  },
+  mounted() {
+    axios({
+      url: "/ncov/index",
+      params: {
+        key: "b1e09762b1a8d164d8c68543cc81142b",
+      },
+    }).then((res) => {
+      console.log(res);
+      this.ncovInfo = {
+        note1: res.data.newslist[0].desc.note1,
+        note2: res.data.newslist[0].desc.note2,
+        note3: res.data.newslist[0].desc.note3,
+        remark1: res.data.newslist[0].desc.remark1,
+        remark2: res.data.newslist[0].desc.remark2,
+        remark3: res.data.newslist[0].desc.remark3,
+      };
+    });
+  },
+  components: {
+    Header,
+    NcovInfo,
+  },
 };
 </script>
